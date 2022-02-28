44da4d0ae8c7cce55a3a6703c56868a79e1d8c9f
Utsav Anand <utsavfrutu@gmail.com>
2022-02-28 19:43:28 +0530

    (Project page) : Side nav update

--- src/components/common/SideNav.jsx
+++ src/components/common/SideNav.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 export default function SideNav(props) {
   return (
-    <nav>
+    <nav className='nav-containerr'>
+      <ul className='side-nav body-txt'>
+        <li>Project details</li>
+        <li>Problem statement</li>
+        <li>Ideation session</li>
+        <li>Competitor research</li>
+        <li>Content map</li>
+        <li>Wireframing</li>
+        <li>UI design</li>
+        <li>Developed application</li>
+        <li>User testing</li>
+        <li>Deployment</li>
+      </ul>
     </nav>
   )
 }
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -5,88 +5,92 @@ import SideNav from '../components/common/SideNav'
 
 export default function ProjectPage(props) {
   return (
-    <section className='project-details'>
+    <>
 
-      <NavBar></NavBar> {/* FIXME: fixed with z-index 2 */}
-
-      <SideNav></SideNav> {/* FIXME: fixed with index z-index 1 */}
+      <SideNav></SideNav> {/*FIXME: fixed with index z-index 1*/}
 
 
+      <section className='project-details'>
 
-      <div className="hero-image flex-center"> {/* FIXME: fixed with z-index 3 */}
-        <img src={PlaceholderImg} alt="Brand identity" />
-      </div>
-
+        {/* FIXME: fixed with z-index 2 */}
 
 
-      <article className='about-project bg-black-gradient container'>
-        <h1 className='heading-txt'>Project title here</h1>
-        <p className='body-txt'>Project short description: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+        <div className="hero-image flex-center"> {/* FIXME: fixed with z-index 3 */}
+          <img src={PlaceholderImg} alt="Brand identity" />
+        </div>
 
-        <div className="project-overview">
-          <div className="project-overview-item">
-            <p>Role</p>
-            <h3>Front-end (react)</h3>
-            <h3>UI Design</h3>
+
+
+        <article className='about-project bg-black-gradient container'>
+          <h1 className='heading-txt'>Project title here</h1>
+          <p className='body-txt'>Project short description: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+
+          <div className="project-overview">
+            <div className="project-overview-item">
+              <p>Role</p>
+              <h3>Front-end (react)</h3>
+              <h3>UI Design</h3>
+            </div>
+            <div className="project-overview-item">
+              <p>Type</p>
+              <h3>Personal (case study)</h3>
+            </div>
+            <div className="project-overview-item">
+              <p>Duration</p>
+              <h3>2 Jan - 28 Jan</h3>
+            </div>
+
           </div>
-          <div className="project-overview-item">
-            <p>Type</p>
-            <h3>Personal (case study)</h3>
+
+        </article>
+
+
+
+        <article className="renders-grid container">
+          <div>
+            <img src="" alt="" />
           </div>
-          <div className="project-overview-item">
-            <p>Duration</p>
-            <h3>2 Jan - 28 Jan</h3>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+        </article>
+
+
+
+        <article className="problem-statement container">
+          <h1 className='heading-txt'>Problem statement</h1>
+          <p className='body-txt'>The brand name XYZ that works as a shopping firm has an old interface which is confusing and requires a lot of click action to perform in order to get the job done.</p>
+
+          <div className="image-comparison">
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+        </article>
+
+
+
+        <article className="ideation container">
+          <h1 className='heading-txt'>Ideation session</h1>
+          <p className='body-txt'>Discussing and researching about the plan(s) to achieve the project goals.</p>
+
+          <div className="image-container">
+            <img src="" alt="" />
           </div>
 
-        </div>
+        </article>
 
-      </article>
+      </section>
 
-
-
-      <article className="renders-grid container">
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <img src="" alt="" />
-        </div>
-      </article>
-
-
-
-      <article className="problem-statement container">
-        <h1 className='heading-txt'>Problem statement</h1>
-        <p className='body-txt'>The brand name XYZ that works as a shopping firm has an old interface which is confusing and requires a lot of click action to perform in order to get the job done.</p>
-
-        <div className="image-comparison">
-          <img src="" alt="" />
-          <img src="" alt="" />
-        </div>
-      </article>
-
-
-
-      <article className="ideation container">
-        <h1 className='heading-txt'>Ideation session</h1>
-        <p className='body-txt'>Discussing and researching about the plan(s) to achieve the project goals.</p>
-
-        <div className="image-container">
-          <img src="" alt="" />
-        </div>
-
-      </article>
-
-    </section>
+    </>
   )
 
 }
