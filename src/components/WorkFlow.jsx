1b56fa138a3774f75cdbe1433384b2898d2c03ca
Utsav Anand <utsavfrutu@gmail.com>
2022-03-25 05:22:17 +0530

    Process flow added, design improvements

--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import FlowDiagram from '../assets/images/process-flow.svg';
 
 export default function WorkFlow() {
+
+
   return (
-    <div></div>
+    <div>
+      <img src={FlowDiagram} alt="Process flow diagram" />
+    </div>
   );
 }
