8d1093bd217aa4db11cf915bb79e5a5cd74e79f9
Utsav Anand <utsavfrutu@gmail.com>
2022-03-31 04:10:02 +0530

    data structure changes

---
+++ src/data/project3.js
@@ -0,0 +1,64 @@
+import UIimg from '../assets/images/UI.jpg';
+import PlaceholderImg from '../assets/images/placeholder.png';
+
+export const project3 = {
+
+  pth: "sdfsdf",
+  id: 3,
+  title: "sdfsdfsf",
+  desc: "sdfsdfsf",
+  // img: PlaceholderImg,
+  img: UIimg,
+
+
+
+  overview: {
+    role: [
+      "Front-end (react)", "UI design"
+    ],
+    tools: [
+      "React", "Adobe XD"
+    ],
+    type: "Personal (case study)",
+    duration: "2 Jan - 22 Jan 2022",
+  },
+
+  promoImg: [{
+      url: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+      alt: ""
+    },
+    {
+      url: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+      alt: ""
+    },
+    {
+      url: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+      alt: ""
+    },
+    {
+      url: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+      alt: ""
+    },
+    {
+      url: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+      alt: ""
+    },
+  ],
+
+
+
+  problemStatement: "The brand name XYZZ that works as a shopping firm has an old interface which is confusing and requires a lot of click action to perform in order to get the job done.",
+
+  compareImg: {
+    oldImg: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg",
+    newImg: "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg"
+  },
+
+
+
+
+  "ideation-section": "Discussing and researching about the plan(s) to achieve the project goals.",
+
+  "ideation-img": ""
+
+}
